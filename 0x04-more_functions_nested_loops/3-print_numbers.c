0993b3bc372c40b0399bd41d6406f2a2ed9bfdcc
zekariasf <zekarias.fikru@gmail.com>
2023-09-16 11:17:53 +0000

    more functions

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_numbers - a function that prints numbers from 0 to 9
+ *
+ *
+ * Returs: void
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
