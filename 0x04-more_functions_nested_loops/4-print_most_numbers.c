dbc187dd94c697547335b40dce2083ef1d1120a9
zekariasf <zekarias.fikru@gmail.com>
2023-09-16 11:33:38 +0000

    more functions

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_numbers - a function that prints numbers from 0 to 9
+ *
+ *
+ * Returs: void
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+	if (i != 50 && i != 52)
+	_putchar(i);
+	}
+	_putchar('\n');
+}
