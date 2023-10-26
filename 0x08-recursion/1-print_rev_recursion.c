45a9d679fe9924401230b0c6a828f590f8c39a85
zekariasf <zekarias.fikru@gmail.com>
2023-10-26 09:47:17 +0000

    recursion project

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _print_rev_recursion - function that prints a string in reverse
+ * @s: string input
+ *
+ * Return: 0
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+
+	}
+	else
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
