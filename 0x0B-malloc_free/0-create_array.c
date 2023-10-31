4531e8982ec17fb736900d509cef5788c6a8aae8
zekariasf <zekarias.fikru@gmail.com>
2023-10-31 19:11:23 +0000

    malloc, free

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -16,7 +16,7 @@ char *create_array(unsigned int size, char c)
 	if (size == 0 || arr == NULL)
 		return (NULL);
 
-	for (i = 0; i <size; i++)
+	for (i = 0; i < size; i++)
 		arr[i] = c;
-	return(arr);
+	return (arr);
 }
