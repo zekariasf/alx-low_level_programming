c9a009dff27e3a90e999cae30d2eb801c8bb186a
zekariasf <zekarias.fikru@gmail.com>
2023-11-08 14:02:44 +0000

    print name

---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,9 @@
+#ifndef FUNPTR_H
+#define FUNPTR_H
+#include<stdlib.h>
+#include<stdio.h>
+int _putchar(char c);
+void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
+int int_index(int *array, int size, int (*cmp)(int));
+#endif
