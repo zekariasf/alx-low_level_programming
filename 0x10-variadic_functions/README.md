3eff335d5dab327f85ee8191566f06f19399070d
zekariasf <zekarias.fikru@gmail.com>
2023-11-09 11:44:55 +0000

    sum them all

---
+++ 0x10-variadic_functions/README.md
@@ -0,0 +1,1 @@
+Variadic functions project readme file
---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,11 @@
+#ifndef VAR_ARG_H
+#define VAR_ARG_H
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdarg.h>
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+
+#endif
